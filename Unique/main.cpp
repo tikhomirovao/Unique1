cebebfd52a2774174a3e80fc3d47bac5809fa934
hawri <hawri@DESKTOP-BMM1386>
2024-04-15 14:44:06 +0300

    Add project files.

---
+++ Unique/main.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	const int n = 10;
+	int arr[n];
+	int minRand = 20, maxRand = 30;
+	for (int i = 0; i < n; )
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		bool Unique = true;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] == arr[i])
+			{
+				Unique = false;
+				break;
+			}
+		}
+		if (Unique)
+		{
+			i++;
+		}
+	}
+	cout << "Уникальные числа: " << tab;
+	for (int i = 0; i < n;i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
